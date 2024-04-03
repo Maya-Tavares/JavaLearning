1fa092d27d41aa81b24272fcd927c5e8b34c527e
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2024-04-02 22:31:09 -0300

    Create exercicio02.java

---
+++ selecao_encadeada/src/exercicio02.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class exercicio02 {
+
+	public static void main(String[] args) {
+
+				// $ 250 diaria do hotel 
+				// taxas de serviço:
+				// $15,5 por d, se d > 15
+				// $36 por d, se d = 15
+				// $58 por d, se d < 15
+		
+				int d;
+				float taxa, total;
+				Scanner in = new Scanner(System.in);
+				
+				System.out.println("Quantos dias o cliente ficou: ");
+				d = (int) in.nextFloat();				
+				
+				if (d > 15) {
+					taxa = (float) (15.5 * d);
+					total = taxa + (250 * d);
+					System.out.println("Taxa ----> " + taxa);
+					System.out.println("Total -----> " + total);
+				}
+				else if  ( d == 15) {
+					taxa = 36 * d;
+					total = taxa + (250 * d);
+					System.out.println("Taxa ----> " + taxa);
+					System.out.println("Total -----> " + total);
+				}
+				else if(d < 15) {
+					taxa = 58 * d;
+					total = taxa + (250 * d);
+					System.out.println("Taxa ----> " + taxa);
+					System.out.println("Total -----> " + total);
+				}
+				
+				
+				
+				
+	}
+
+}
