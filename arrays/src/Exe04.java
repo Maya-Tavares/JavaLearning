f64037a25e899c3c8adf8aac06511b922869a829
Maya Tavares <94256355+MayaTavaresIFSP@users.noreply.github.com>
2024-08-20 22:41:32 -0300

    Create Exe04.java

---
+++ arrays/src/Exe04.java
@@ -0,0 +1,9 @@
+
+public class Exe04 {
+
+	public static void main(String[] args) {
+		//  Escreva um programa que receba a temperatura média de cada mês do ano passado e armazene-as em uma lista (vetor). Em seguida, calcule a média anual das temperaturas e mostre todas as temperaturas acima da média anual, e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
+
+	}
+
+}
