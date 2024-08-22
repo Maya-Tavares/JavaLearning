9f8a3a57037e4a3370e271fc06276d50c3a7737f
Maya Tavares <94256355+MayaTavaresIFSP@users.noreply.github.com>
2024-08-21 22:33:22 -0300

    Create Exe03.java

---
+++ selecao_encadeada/src/Exe03.java
@@ -0,0 +1,9 @@
+
+public class Exe03 {
+
+	public static void main(String[] args) {
+		//  Escreva um programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno, imprima o número de alunos com média maior ou igual a 6.0.
+
+	}
+
+}
