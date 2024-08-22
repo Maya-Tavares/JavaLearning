b50e61f14a6b6543fb3c2cb61892802f567154cc
Maya Tavares <94256355+MayaTavaresIFSP@users.noreply.github.com>
2024-08-21 22:33:17 -0300

    Create Exe011.java

---
+++ selecao_encadeada/src/Exe011.java
@@ -0,0 +1,14 @@
+
+public class Exe011 {
+
+	public static void main(String[] args) {
+		//  Escreva um programa que leia e armazene os nomes dos arquivos e os respectivos
+		// tamanhos em bytes. Imprima uma listagem contendo os nomes dos arquivos e seus
+//		respectivos tamanhos em megabyte e gigabyte.
+//		1 Mebagyte (1MB) = 1.048.576 (220 bytes) = 1024 * 1024
+//		1 Gigabyte (1GB) = 1.073.741.824 (230 bytes) = 1024 * 1024 * 1024
+
+			
+	}
+
+}
