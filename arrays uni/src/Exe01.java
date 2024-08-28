7809f35f8007734dc42f3c9f11a626f4f63dd559
Maya Tavares <94256355+MayaTavaresIFSP@users.noreply.github.com>
2024-08-27 22:43:36 -0300

    Create Exe01.java

---
+++ arrays uni/src/Exe01.java
@@ -0,0 +1,12 @@
+
+public class Exe01 {
+
+	public static void main(String[] args) {
+		// Escreva um programa em Java que preencha um vetor de 10 posições com valores
+		//fornecidos pelo usuário. Imprima no vídeo o maior e o menor valor armazenado.
+		//Observação: maior valor inteiro é Integer.MAX_VALUE e o menor valor inteiro é
+		// Integer.MIN_VALUE
+
+	}
+
+}
