509e89a807eb161ffc92a4f666ddd17db9bdb251
Maya Tavares <94256355+MayaTavaresIFSP@users.noreply.github.com>
2024-08-27 22:43:40 -0300

    Create Exe02.java

---
+++ arrays uni/src/Exe02.java
@@ -0,0 +1,10 @@
+
+public class Exe02 {
+
+	public static void main(String[] args) {
+		// Escreva um programa em Java que preencha um vetor de 10 posições com valores
+		// fornecidos pelo usuário. Imprima no vídeo a quantidade de números pares e ímpares digitados.
+
+	}
+
+}
